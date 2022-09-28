e07b5ad7fcc6ab953caba6b050b113ec3928328e
snajperqs <105291533+snajperqs@users.noreply.github.com>
2022-09-28 21:03:29 +0100

    zmiana archow na inne nazwy

--- src/MapPoints.js
+++ src/MapPoints.js
@@ -20,67 +20,67 @@
 //pointArch.addImage("img/arch (5).jpg");
 //pointArch.bind();
 
-var pointArch = new PointOfInterest("palace");
-pointArch.title = "Palace Duques de Bragança";
-pointArch.description = "Pałac";
-pointArch.coordinates = [41.446584637897686, -8.291293842727809];
-pointArch.addImage("img/arch (5).jpg");
-pointArch.bind();
+var pointPalace = new PointOfInterest("palace");
+pointPalace.title = "Palace Duques de Bragança";
+pointPalace.description = "Pałac";
+pointPalace.coordinates = [41.446584637897686, -8.291293842727809];
+pointPalace.addImage("img/arch (5).jpg");
+pointPalace.bind();
 
-var pointArch = new PointOfInterest("castle");
-pointArch.title = "Castelo de Guimaraesa";
-pointArch.description = "zamek";
-pointArch.coordinates = [41.44792511998326, -8.29030888408886];
-pointArch.addImage("img/arch (5).jpg");
-pointArch.bind();
+var pointCastle = new PointOfInterest("castle");
+pointCastle.title = "Castelo de Guimaraesa";
+pointCastle.description = "zamek";
+pointCastle.coordinates = [41.44792511998326, -8.29030888408886];
+pointCastle.addImage("img/arch (5).jpg");
+pointCastle.bind();
 
-var pointArch = new PointOfInterest("temple");
-pointArch.title = "Igreja Nossa Senhora do Carmo";
-pointArch.description = "Świątynia";
-pointArch.coordinates = [41.4459378663381, -8.292455009259674];
-pointArch.addImage("img/arch (5).jpg");
-pointArch.bind();
+var pointTemple = new PointOfInterest("temple");
+pointTemple.title = "Igreja Nossa Senhora do Carmo";
+pointTemple.description = "Świątynia";
+pointTemple.coordinates = [41.4459378663381, -8.292455009259674];
+pointTemple.addImage("img/arch (5).jpg");
+pointTemple.bind();
 
-var pointArch = new PointOfInterest("cathedral");
-pointArch.title = "Braga Cathedral";
-pointArch.description = "Świątynia";
-pointArch.coordinates = [41.55030835647579, -8.426865574301342];
-pointArch.addImage("img/arch (5).jpg");
-pointArch.bind();
+var pointCathedral = new PointOfInterest("cathedral");
+pointCathedral.title = "Braga Cathedral";
+pointCathedral.description = "Świątynia";
+pointCathedral.coordinates = [41.55030835647579, -8.426865574301342];
+pointCathedral.addImage("img/arch (5).jpg");
+pointCathedral.bind();
 
-var pointArch = new PointOfInterest("schody");
-pointArch.title = "Bom Jesus Staircases";
-pointArch.description = "Świątynia";
-pointArch.coordinates = [41.554593556888804, -8.378757012940845];
-pointArch.addImage("img/arch (5).jpg");
-pointArch.bind();
+var pointStairs = new PointOfInterest("schody");
+pointStairs.title = "Bom Jesus Staircases";
+pointStairs.description = "Świątynia";
+pointStairs.coordinates = [41.554593556888804, -8.378757012940845];
+pointStairs.addImage("img/arch (5).jpg");
+pointStairs.bind();
 
-var pointArch = new PointOfInterest("kosciolBraga");
-pointArch.title = "Bom Jesus do monte";
-pointArch.description = "Świątynia";
-pointArch.coordinates = [41.55469794500868, -8.376364491341201];
-pointArch.addImage("img/arch (5).jpg");
-pointArch.bind();
+var pointBragaChurch = new PointOfInterest("kosciolBraga");
+pointBragaChurch.title = "Bom Jesus do monte";
+pointBragaChurch.description = "Świątynia";
+pointBragaChurch.coordinates = [41.55469794500868, -8.376364491341201];
+pointBragaChurch.addImage("img/arch (5).jpg");
+pointBragaChurch.bind();
 
-var pointArch = new PointOfInterest("centrumGumaires");
-pointArch.title = "Bom Jesus do monte";
-pointArch.description = "Świątynia";
-pointArch.coordinates = [41.44278536234879, -8.292722820333019];
-pointArch.addImage("img/arch (5).jpg");
-pointArch.bind();
+var pointCentre = new PointOfInterest("centrumGumaires");
+pointCentre.title = "Bom Jesus do monte";
+pointCentre.description = "Świątynia";
+pointCentre.coordinates = [41.44278536234879, -8.292722820333019];
+pointCentre.addImage("img/arch (5).jpg");
+pointCentre.bind();
 
-var pointArch = new PointOfInterest("dworzecPKP");
-pointArch.title = "Porto-São Bento";
-pointArch.description = "Świątynia";
-pointArch.coordinates = [41.14566631499273, -8.610533049447204];
-pointArch.addImage("img/arch (5).jpg");
-pointArch.bind();
+var pointDworzec = new PointOfInterest("dworzecPKP");
+pointDworzec.title = "Porto-São Bento";
+pointDworzec.description = "Świątynia";
+pointDworzec.coordinates = [41.14566631499273, -8.610533049447204];
+pointDworzec.addImage("img/arch (5).jpg");
+pointDworzec.bind();
 
-var pointArch = new PointOfInterest("kosciolPorto");
-pointArch.title = "Kościół św. Ildefonsa w Porto";
-pointArch.description = "Świątynia";
-pointArch.coordinates = [41.14617272417474, -8.606673885626677];
-pointArch.addImage("img/arch (5).jpg");
-pointArch.bind();
+var pointPortoChurch = new PointOfInterest("kosciolPorto");
+pointPortoChurch.title = "Kościół św. Ildefonsa w Porto";
+pointPortoChurch.description = "Świątynia";
+pointPortoChurch.coordinates = [41.14617272417474, -8.606673885626677];
+pointPortoChurch.addImage("img/arch (5).jpg");
+pointPortoChurch.bind();
 
 
